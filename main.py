4c6dcc22b7a4068db253cabbf991afc1457b507a
tristan sebillet <tristan.sebillet@bts-malraux.net>
2014-09-23 14:08:11 +0000

    implémentation arguments

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("echo")
-args = parser.parse_ars()
+
+parser.add_argument("nom_playlist")
+parser.add_argument("temps")
+args = parser.parse_args()
+
+print(args.nom_playlist)
+print"Votre playlist a pour duree: "+(args.temps)
