47d25568e3e1bb79442d29223e449c5ae3656fa3
Tristan Sebillet <tristan.sebillet@bts-malraux.net>
2014-10-01 13:02:56 +0000

    ajout entre 0 et 100

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ parser.add_argument("nom_playlist", help="Veuillez saisir le nom de votre playli
 
 parser.add_argument("temps",type=int,help="Veuillez saisir la duree de votre playlist")
 
-parser.add_argument("--titre",help="Saisi d'un titre de morceau inclus dans la playlist")
+parser.add_argument("--titre",nargs=2,help="Saisi d'un titre de morceau inclus dans la playlist et de son pourcentage")
 
-parser.add_argument("--genre",nargs=2,help="Saisi d'un genre a inclure dans la playlist")
+parser.add_argument("--genre",nargs=2,help="Saisi d'un genre a inclure dans la playlist et de son pourcentage")
 
-parser.add_argument("--sousgenre",nargs=2,help="Saisi d'un sous-genre a inclure dans la playlist")
+parser.add_argument("--sousgenre",nargs=2,help="Saisi d'un sous-genre a inclure dans la playlist et de son pourcentage")
 
-parser.add_argument("--artiste",nargs=2,help="Saisi d'un artiste inclus dans la playlist")
+parser.add_argument("--artiste",nargs=2,help="Saisi d'un artiste inclus dans la playlist et de son pourcentage")
 
-parser.add_argument("--album",nargs=2,help="Saisi d'un album inclus dans la playlist")
+parser.add_argument("--album",nargs=2,help="Saisi d'un album inclus dans la playlist et de son pourcentage")
 
 parser.add_argument("--format", choices=['m3u','xspf'])
 
@@ -32,7 +32,7 @@ def checkSousArgs2(arg):
     try:
         nb = int(arg[1])
         if (checkIntNatural(nb) == False):
-            print ("La quantité doit être positive ! (" + arg[1] + ")")
+            print ("La quantité doit être positive et inférieur ou égal a 100! (" + arg[1] + ")")
             logging.warning('La quantité saisie doit etre positive')
             exit(1)
         return 0
@@ -43,18 +43,17 @@ def checkSousArgs2(arg):
         exit(1)
 ''' Vérifie qu'un nombre est un entier naturel '''
 def checkIntNatural(nb):
-    if (nb > 0) :
+    if (nb > 0 & nb<100) :
         return True
     else:
         return False
-
-
 
 '''affichage des arguments saisis'''
 print(args.nom_playlist+" a pour duree: "+str(args.temps)+" minutes.")
 
 '''test si les arguments son saisis et si oui affichage de ceux-ci'''
 if args.titre:
+    checkSousArgs2(args.titre)
     print("titre choisi:"+(args.titre))
 
 if args.genre:
@@ -75,7 +74,7 @@ if args.album :
 
 if args.format:
     print("format choisi:"+(args.format) )
-
+logging.info("Affichage terminé")
 
 
 
